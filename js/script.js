98d9ca492b18723748ca6c34995c687f503e5b31
Alexey <shev.alexey.it@gmail.com>
2021-10-27 13:16:01 +0300

    New task on line 1-15

--- js/script.js
+++ js/script.js
@@ -1,63 +1,20 @@
 /* Задания на урок:
 
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
 
-2) Изменить жанр фильма, поменять "комедия" на "драма"
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
 
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
 
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
 
-5) Добавить нумерацию выведенных фильмов */
+5) Фильмы должны быть отсортированы по алфавиту */
 
 'use strict';
 
-const adv = document.querySelectorAll('.promo__adv img');
-const genre = document.querySelector('.promo__genre');
-const poster = document.querySelector(".promo__bg");
-const listOfFilms = document.querySelector(".promo__interactive-list");
-
-adv.forEach(item => {
-    item.remove();
-});
-
-listOfFilms.innerHTML = "";
-
-genre.textContent = "ДРАМА";
-
-poster.style.backgroundImage = "url('img/bg.jpg')";
-
-console.log(poster.innerHTML)
-
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
-
-let filmName;
-let count;
-
-movieDB.movies.sort().forEach((element, index) => {
-
-    listOfFilms.innerHTML += `
-    <li class="promo__interactive-item">${++index}. ${element}
-        <div class="delete"></div>
-    </li>
-    `
-
-    // const li = document.createElement("li");
-    // let text = document.createTextNode(element);
-    // li.appendChild(text);
-    // li.classList.add("promo__interactive-item");
-    // console.log(li.prepend(count));
-    // listOfFilms.appendChild(li);
-});
-
-
+// Возьмите свой код из предыдущей практики
